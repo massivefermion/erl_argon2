66a1561d7d03cf75be6a068a04f75021544fb4f2
shayan javani <massivefermion@protonmail.com>
2024-03-23 13:53:54 +0330

    updated deps, minor changes

--- src/argon2.erl
+++ src/argon2.erl
@@ -1,9 +1,13 @@
 -module(argon2).
 
--export([hash/1, hash/2, hash_with_secret/2, hash_with_secret/3, verify/2,
-         verify_with_secret/3]).
+-export([
+    hash/1, hash/2,
+    hash_with_secret/2, hash_with_secret/3,
+    verify/2,
+    verify_with_secret/3
+]).
 
--on_load init/0.
+-on_load(init/0).
 
 -define(APPNAME, ?MODULE).
 -define(LIBNAME, "argon2").
@@ -27,21 +31,25 @@ verify_with_secret(_, _, _) ->
     not_loaded(?LINE).
 
 init() ->
-    SoName =
-        case code:priv_dir(?APPNAME) of
-            {error, bad_name} ->
-                case filelib:is_dir(
-                         filename:join(["..", priv]))
-                of
-                    true ->
-                        filename:join(["..", priv, ?LIBNAME]);
-                    _ ->
-                        filename:join([priv, ?LIBNAME])
-                end;
-            Dir ->
-                filename:join(Dir, ?LIBNAME)
-        end,
+    SoName = get_so_name(),
     erlang:load_nif(SoName, 0).
 
 not_loaded(Line) ->
     exit({not_loaded, [{module, ?MODULE}, {line, Line}]}).
+
+get_so_name() ->
+    case code:priv_dir(?APPNAME) of
+        {error, bad_name} ->
+            case
+                filelib:is_dir(
+                    filename:join(["..", priv])
+                )
+            of
+                true ->
+                    filename:join(["..", priv, ?LIBNAME]);
+                _ ->
+                    filename:join([priv, ?LIBNAME])
+            end;
+        Dir ->
+            filename:join(Dir, ?LIBNAME)
+    end.
